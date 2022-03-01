c86cf210d083b4bc09ac23903a12e06db90b30bb
Lukaspds91 <99550960+Lukaspds91@users.noreply.github.com>
2022-03-01 15:07:12 -0300

    Add files via upload

---
+++ IdadeDiaMesAno.java
@@ -0,0 +1,30 @@
+package br.com.generation.aula01;
+import java.util.Scanner;
+
+/*1. Faça um sistema que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a
+  expressa apenas em dias.*/
+
+public class IdadeDiaMesAno {
+	public static void main(String[] args){
+		final short DIAS_NO_ANO = 365;
+		final short DIAS_NO_MES = 30;
+		short dias;
+		short meses;
+		short anos;
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Informe a sua idade.");
+		System.out.print("Anos: ");
+		anos = leitor.nextShort();
+		
+		System.out.print("Meses: ");
+		meses = leitor.nextShort();
+		
+		System.out.print("Dias: ");
+		dias = leitor.nextShort();
+		
+		dias += (anos * DIAS_NO_ANO) + (meses * DIAS_NO_MES);
+		
+		System.out.println("\n\nA sua idade em dias é " + dias);
+	}
+}
